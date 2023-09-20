2bb924e0af756c41a4033e44c3fd030ab0449427
gedons <gedoniani@gmail.com>
2023-09-20 23:06:53 +0100

    db environment variables

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const dbConfig = {
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'fluxcontact',
+  host: 'dpg-ck5j3bei9prc73ck1rc0-a',
+  user: 'flux_contact_user',
+  password: '3DExqzi0EWWuqHbM646g5YbhkSO1ONDh',
+  database: 'flux_contact',
 };
 
 const pool = mysql.createPool(dbConfig);
